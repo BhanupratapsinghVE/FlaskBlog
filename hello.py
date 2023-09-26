94a85b5fac76e07549641a290bb11c1c8e2f5764
BhanupratapsinghVE <bhanuve2001@gmail.com>
2023-09-26 11:39:53 +0530

    initial commit

---
+++ hello.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    first_name = "John"
+    pizzas = ["Pepperoni", "cheese", "Mushrooms"]
+    return render_template("index.html", first_name=first_name, pizzas=pizzas)
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template("user.html", name=name)
+
+
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+# Internal Server Error URL
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
